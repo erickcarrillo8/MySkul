ebea8dfd807f1be57a5b9dd1a8531b06d2bf62f9
Moyses Nev <moyrene84@gmail.com>
2016-09-25 20:15:12 -0700

    Entities Alumno

---
+++ AppTemplate/App.Entities/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public int NoControl { get; set; }
+        public DateTime FechaDeNamiento { get; set; }
+        public byte[] Foto { get; set; }
+        public bool Jefe { get; set; }
+    }
+}
