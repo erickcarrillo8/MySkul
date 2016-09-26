bef25917715dcddc85cfca76e4756ea5692fbe8d
FAMILIA OSUNA <FAMILIA OSUNA>
2016-09-26 15:58:40 -0700

    Entities Aviso

---
+++ Avisos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Avisos
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaHora { get; set; }
+    }
+}
