8574bc98f75d3f5b63ce803d3d27e3ec2cd9433e
FAMILIA OSUNA <FAMILIA OSUNA>
2016-09-26 16:02:33 -0700

    Entities Publicacion

---
+++ Publicacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Publicacion
+
+
+    { 
+        public int IDDocente { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaHora { get; set; }
+    }
+}
