a3601c6f7aafb1486ac8537635263ecdc7e6ff67
Moyses Nev <moyrene84@gmail.com>
2016-09-25 20:16:48 -0700

    Entities Docente

---
+++ AppTemplate/App.Entities/Docente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Docente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public byte[] Foto { get; set; }
+
+    }
+}
