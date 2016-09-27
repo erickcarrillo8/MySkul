8aceb1bd8a9503941702e3d93e622062f4507f5b
Moyses Nev <moyrene84@gmail.com>
2016-09-26 21:28:42 -0700

    Entities Clase

---
+++ AppTemplate/App.Entities/Clase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Clase
+    {
+        public int Id_Materia { get; set; }
+        public int Id_Docente { get; set; }
+    }
+}
