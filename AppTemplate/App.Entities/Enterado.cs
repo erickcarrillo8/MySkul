1e1d23281691c2111b1ef77b86221e8b03524b35
Moyses Nev <moyrene84@gmail.com>
2016-09-26 21:55:32 -0700

    Entities Enterado

---
+++ AppTemplate/App.Entities/Enterado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Enterado
+    {
+        public int Id_Alumno { get; set; }
+        public int Id_Aviso { get; set; }
+    }
+}
