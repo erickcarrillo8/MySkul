b39854ec9de4ce0dd86b69a6d4dea6df9206d27c
Moyses Nev <moyrene84@gmail.com>
2016-09-26 21:25:00 -0700

    Entities Alumnos_Clase

---
+++ AppTemplate/App.Entities/Alumnos_Clase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    public class Alumnos_Clase
+    {
+        public int Id_Alumno { get; set; }
+        public int Id_Clase { get; set; }
+    }
+}
