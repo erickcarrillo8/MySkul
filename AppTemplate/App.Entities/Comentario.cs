0af89c33273c19c02b07d02e9c98b57c547226ba
salma <danielamontano2102@gmail.com>
2016-09-27 19:15:42 -0700

    Comentario2

--- AppTemplate/App.Entities/Comentario.cs
+++ AppTemplate/App.Entities/Comentario.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.Entities
 {
     public class Comentario
     {
+        [Key]
         public int Id { get; set; }
         public int Id_Alumno { get; set; }
         public int Id_Aviso { get; set; } 
