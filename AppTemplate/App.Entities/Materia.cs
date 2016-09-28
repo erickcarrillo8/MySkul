27902c1364b793d32b0ac74c99d70a2b735502c9
Moyses Nev <moyrene84@gmail.com>
2016-09-27 21:41:56 -0700

    Entities Materia Actualizado

--- AppTemplate/App.Entities/Materia.cs
+++ AppTemplate/App.Entities/Materia.cs
@@ -8,7 +8,7 @@ namespace App.Entities
 {
     public class Materia
     {
-        public int Id_ { get; set; }
-        public string nombre { get; set; }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
     }
 }
